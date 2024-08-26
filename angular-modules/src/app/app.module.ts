2fcee901e84fb2ed99e5f57fef8e33a05fbb9c68
viktorskomorokhov <viktor.skomorokhov.tech@gmail.com>
2024-08-26 22:32:55 +0300

    Модули ангуляр. пример с общим модулем

--- angular-modules/src/app/app.module.ts
+++ angular-modules/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InformationComponent } from './modules/information/information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InformationComponent
   ],
   imports: [
     BrowserModule,
